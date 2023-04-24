c483f650e2f3b496bec3b38d6cf79615413d0bd8
Choseongjin <sjcho@vantus.co.kr>
2023-04-24 11:25:04 +0900

    first commit

---
+++ api/src/main/java/com/aws/api/main/MainController.java
@@ -0,0 +1,24 @@
+package com.aws.api.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/login/{userId}/{password}")
+    public Map<String, Object> login(@PathVariable String userId, @PathVariable String password) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		String resultCode = "-9999";
+		
+		if ("csj".equals(userId) && "1".equals(password)) {
+			resultCode = "0000";
+		}
+		result.put("resultCode", resultCode);
+        return result;
+    }
+}
