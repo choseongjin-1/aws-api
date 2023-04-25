a6a9e6ba871e150b7d0f50a9cea8c31bf9c48d3d
Choseongjin <sjcho@vantus.co.kr>
2023-04-25 09:39:20 +0900

    api url 수정

--- api/src/main/java/com/aws/api/main/MainController.java
+++ api/src/main/java/com/aws/api/main/MainController.java
@@ -5,12 +5,14 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1")
 public class MainController {
 
-	@GetMapping("/login/{userId}/{password}")
+	@GetMapping("/users/{userId}/{password}")
     public Map<String, Object> login(@PathVariable String userId, @PathVariable String password) {
 		Map<String, Object> result = new HashMap<String, Object>();
 		String resultCode = "-9999";
